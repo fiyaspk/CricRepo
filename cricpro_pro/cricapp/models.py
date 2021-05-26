d1ac06c0edad8dc446e45a3194533fb895e5daa1
fiyas <fiyaspk1820@gmail.com>
2021-05-26 09:36:50 +0530

    commit1

--- cricpro_pro/cricapp/models.py
+++ cricpro_pro/cricapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+class Register(models.model):
+    player_name=models.CharField(max_length=50)
+    place=models.CharField(max_length=50)
+    DOB=models.DateField()
+    role=models.CharField(max_length=20)
+    email=models.CharField(max_length=50)
+    mobile=models.BigIntegerField()
 
-# Create your models here.
+
+
